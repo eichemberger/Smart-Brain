f25ad681f54bf1dcfbbe8127bfa458240b0d5b17
German Eichemberger <germaneichemberger@gmail.com>
2023-04-17 22:38:31 -0300

    removes hardcoded api key

--- server/controllers/image.js
+++ server/controllers/image.js
@@ -2,7 +2,7 @@ const Clarifai = require('clarifai');
 
 //You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
-  apiKey: '73846a8538104757b357a62e75642735',
+  apiKey: process.env.CLARIFAI_API_KEY,
 });
 
 const handleApiCall = (req, res) => {
